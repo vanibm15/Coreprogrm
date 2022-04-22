6b609729102c1e6ad342678bf1a7a9f4fd7a6fb1
vanibm15 <vanibm15@gmail.com>
2022-04-22 17:04:01 +0530

    added:checking of vowel & consonant

--- src/com/coreprogrm/Coreprogrm.java
+++ src/com/coreprogrm/Coreprogrm.java
@@ -4,14 +4,15 @@ import java.util.Scanner;
 
 public class Coreprogrm {
     public static void main(String[] args) {
-        int num;
-        System.out.println("enter an number :");
-        Scanner scanner = new Scanner(System.in);
-        num = scanner.nextInt();
-        if((num % 2 == 0) )
 
-        System.out.println("number is even ");
-else
-        System.out.println("number is odd");
+        char ch = 'i';
+
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
+            System.out.println(ch + " is vowel");
+        else
+
+            System.out.println(ch + " is consonant");
+
+
     }
 }
